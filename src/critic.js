f45491366cba627654f439024f62142571d3b484
Hendrik Erz <hendrik@zettlr.com>
2025-10-25 10:23:00 +0200

    feat: Properly emit Content nodes

--- src/critic.js
+++ src/critic.js
@@ -3,16 +3,16 @@ import {LRParser, LocalTokenGroup} from "@lezer/lr"
 import {criticHighlighting} from "./highlight.js"
 export const parser = LRParser.deserialize({
   version: 14,
-  states: "%WQQOPOOOfOPO'#C^OnOPO'#CaOvOPO'#CdO!OOPO'#ChO!WOPO'#CkOOOO'#Cq'#CqOOOO'#Cn'#CnQQOPOOOOOO'#Co'#CoO!`OPO,58xOOOO,58x,58xO!hOPO,58{OOOO,58{,58{O!pOPO,59OO!xOPO,59OO#QOPO,59SOOOO,59S,59SO#YOPO,59VOOOO,59V,59VOOOO-E6l-E6lOOOO-E6m-E6mOOOO1G.d1G.dOOOO1G.g1G.gO#bOPO1G.jO#bOPO1G.jOOOO1G.j1G.jOOOO1G.n1G.nOOOO1G.q1G.qO#jOPO7+$UOOOO7+$U7+$UOOOO<<Gp<<Gp",
-  stateData: "#r~ORPOUQOXRO]SO`TOfUO~OSZOfXO~OV]OfXO~OY_OfXO~O^aOfXO~OacOfXO~OSfOfXO~OVgOfXO~OYhOfXO~OZjOfXO~O^kOfXO~OalOfXO~OZnOfXO~OZoOfXO~O",
-  goto: "!efPPgPPgPPgPPPgPPgPPkqP!aTUOWQWORdWQYPQ[QQ^RQ`SQbT^eY[^`bimQi_RmhTVOW",
-  nodeNames: "⚠ CriticMarkup Addition AdditionStart AdditionEnd Deletion DeletionStart DeletionEnd Substitution SubstitutionStart SubstitutionMiddle SubstitutionEnd Highlight HighlightStart HighlightEnd Comment CommentStart CommentEnd",
+  states: "%WQQOPOOOfOPO'#C^OnOPO'#CbOvOPO'#CeO!OOPO'#CiO!WOPO'#ClOOOO'#Cr'#CrOOOO'#Co'#CoQQOPOOOOOO'#Cp'#CpO!`OPO,58xOOOO,58x,58xO!hOPO,58|OOOO,58|,58|O!pOPO,59PO!xOPO,59PO#QOPO,59TOOOO,59T,59TO#YOPO,59WOOOO,59W,59WOOOO-E6m-E6mOOOO-E6n-E6nOOOO1G.d1G.dOOOO1G.h1G.hO#bOPO1G.kO#bOPO1G.kOOOO1G.k1G.kOOOO1G.o1G.oOOOO1G.r1G.rO#jOPO7+$VOOOO7+$V7+$VOOOO<<Gq<<Gq",
+  stateData: "#r~ORPOSUOVQOYRO^SOaTO~OSXOTZO~OSXOW]O~OSXOZ_O~OSXO_aO~OSXObcO~OSXOTfO~OSXOWgO~OSXOZhO~OSXO[jO~OSXO_kO~OSXOblO~OSXO[nO~OSXO[oO~O",
+  goto: "!fgPPhPPPhPPhPPPhPPhPPlrP!bTUOWQWORdWQYPQ[QQ^RQ`SQbT^eY[^`bimQi_RmhTVOW",
+  nodeNames: "⚠ CriticMarkup Addition AdditionStart Content AdditionEnd Deletion DeletionStart DeletionEnd Substitution SubstitutionStart SubstitutionMiddle SubstitutionEnd Highlight HighlightStart HighlightEnd Comment CommentStart CommentEnd",
   maxTerm: 22,
   propSources: [criticHighlighting],
   skippedNodes: [0],
   repeatNodeCount: 2,
   tokenData: "RORO",
-  tokenizers: [new LocalTokenGroup("%S~RU{|e}!Ov!^!_!X!_!`!j#o#p!{#r#s$h~hP{|k~nP#q#rq~vOS~~yP}!O|~!PP#q#r!S~!XOV~~![P!^!_!_~!bP#q#r!e~!jOa~~!mP!_!`!p~!sP#q#r!v~!{O^~~#OT{|#_}!O#j!_!`#u!`!a$Q#r#s$]~#bP{|#e~#jOR~~#mP}!O#p~#uOU~~#xP!_!`#{~$QO]~~$TP!`!a$W~$]O`~~$`P#r#s$c~$hOX~~$kQ!`!a$q#r#s$v~$vOY~~$yP#q#r$|~%ROZ~~", 187, 22)],
+  tokenizers: [new LocalTokenGroup("%S~RU{|e}!Ov!^!_!X!_!`!j#o#p!{#r#s$h~hP{|k~nP#q#rq~vOT~~yP}!O|~!PP#q#r!S~!XOW~~![P!^!_!_~!bP#q#r!e~!jOb~~!mP!_!`!p~!sP#q#r!v~!{O_~~#OT{|#_}!O#j!_!`#u!`!a$Q#r#s$]~#bP{|#e~#jOR~~#mP}!O#p~#uOV~~#xP!_!`#{~$QO^~~$TP!`!a$W~$]Oa~~$`P#r#s$c~$hOY~~$kQ!`!a$q#r#s$v~$vOZ~~$yP#q#r$|~%RO[~~", 187, 4)],
   topRules: {"CriticMarkup":[0,1]},
   tokenPrec: 0
 })
